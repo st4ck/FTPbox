0d7a539b8ddd07dd1847432d00eeac7a9939017b
Roberto Tacconelli <st4ck@live.com>
2017-10-23 16:19:52 +0200

    Fix throttle transfer limits

--- FTPboxLib/Client.cs
+++ FTPboxLib/Client.cs
@@ -723,7 +723,7 @@ namespace FTPboxLib
             var rate = (int) (elapsed.TotalSeconds < 1 ? transfered : transfered/elapsed.TotalSeconds);
             if (limit > 0 && rate > 1000*limit)
             {
-                double millisecDelay = (transfered/limit - elapsed.Milliseconds);
+                double millisecDelay = (transfered/limit - elapsed.TotalMilliseconds);
 
                 if (millisecDelay > Int32.MaxValue)
                     millisecDelay = Int32.MaxValue;
